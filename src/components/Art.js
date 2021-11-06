0b76166e3a68e3c0383f50ed4998db4fa4955f8f
Elana R <eryzna@gmail.com>
2021-11-05 23:11:46 -0400

    add nav on new pages

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Nav from './Nav'
 
 export default class Art extends Component {
 
     render() {
         return (
+            <>
+            <Nav />
             <h1> ART PAGE
             </h1>
+            </>
 
         )
     }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Nav from './Nav'
 
 export default class Contact extends Component {
 
     render() {
         return (
+            <>
+            <Nav />
             <h1> YOOOOOOOOOOO
             </h1>
+            </>
 
         )
     }
