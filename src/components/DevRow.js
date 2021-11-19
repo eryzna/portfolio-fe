74d7fc973689d60695e8a18ad2fc75d860fceffe
Elana R <eryzna@gmail.com>
2021-11-19 18:42:08 -0500

    dev page img placeholder

--- src/components/DevRow.js
+++ src/components/DevRow.js
@@ -10,7 +10,17 @@ export default class DevRow extends Component {
       return (
 
           <div class="container-fluid">
-            <h1>Development page forthcoming...</h1>  
+            <div class="row">
+              <div class="col grid-border">
+                <h1>Development page forthcoming...</h1>  
+              </div>
+            </div>
+            <div class="row">
+              <div class="col grid-border">
+                <img class="img-fluid" src="portfolio-fe/Gustav-1.png" width="57%"></img>
+              </div>
+            </div>
+            
           </div>
 
       )
