e885ff972bd716a59cc0255e78b93ba3c8483d1e
Elana R <eryzna@gmail.com>
2021-11-24 10:53:47 -0500

    responsive nested images

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Nav from './Nav'
 
-export default class Contact extends Component {
+export default class About extends Component {
 
     render() {
         return (
--- src/components/RowTwo.js
+++ src/components/RowTwo.js
@@ -43,7 +43,7 @@ export default class RowTwo extends Component {
                       <h2 class="grid-text"> Greetings! My name is Elana. I am a full-stack developer and artist.</h2>
                     </div>
                     <div class="pb-3 nested-rows" id="nested-7">
-                      <img class="img-fluid skills-icons" src="/portfolio-fe/Skills3.png"></img>
+                      <img class="skills-icons" src="/portfolio-fe/Skills3.png" width="100%"></img>
                     </div>
                     <div class="row nested-rows">
                       <div class="col-md-6 themed-grid-col" id="nested-6-1">
@@ -57,7 +57,7 @@ export default class RowTwo extends Component {
                 
                 <div class="col-md-4 themed-grid-col" id="nested-4">
                   <div class="pb-3 nested-rows" id="nested-9">
-                      <img class="img-fluid" src="/portfolio-fe/Sunset.png"></img>
+                      <img class="" src="/portfolio-fe/Sunset.png" width="100%" height="84px"></img>
                     </div> 
                   <div class= "grid-border" id="art-label">
                     <h4>Art</h4>
